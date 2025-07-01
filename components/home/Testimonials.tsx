f3d3d1524ef6e751c01547698a9d2031074ac8cc
Jheus Reyes <jheusreyes213@gmail.com>
2025-07-01 21:49:42 +0800

    Fix: handled designs page rendering issue

--- components/home/Testimonials.tsx
+++ components/home/Testimonials.tsx
@@ -4,7 +4,7 @@ import SectionTitle from "../global/SectionTitle";
 function Testimonials() {
   return (
     <div className="flex flex-col text-left max-w-md md:max-w-5xl w-full m-auto px-4 py-16">
-      <SectionTitle title="Testimonials" />
+     
     </div>
   );
 }
